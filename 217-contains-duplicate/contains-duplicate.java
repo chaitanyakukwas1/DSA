814c16149e8d708e0099dce7dd00f534971b5801
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-13 21:39:25 +0530

    Time: 14 ms (89.35%) | Memory: 93.5 MB (51.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,29 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i+1])
+    // public boolean containsDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i=0;i<nums.length-1;i++)
+    //     {
+    //         if(nums[i]==nums[i+1])
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+        public boolean containsDuplicate(int[] nums) {
+            
+            Set<Integer> set = new HashSet<>();
+
+            for(int i=0;i<nums.length;i++)
             {
-                return true;
+                if(set.contains(nums[i]))
+                {
+                    return true;
+                }
+                set.add(nums[i]);
             }
+            return false;
         }
-        return false;
-    }
+
 }
