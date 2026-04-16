f2097a87a6bbb146f170dcdbfa37d892ed2ea228
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-16 23:55:14 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (56.13%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,33 +1,58 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         
-        int n = nums.length;
-        int l = 0,
-            r = nums.length-1;
+        int res[] = new int[nums.length];
 
-        int[] result = new int[n];
-
-        for(int i=n-1;i>=0;i--)
-        {
-            int val;
-
-            if(Math.abs(nums[l])>Math.abs(nums[r]))
-            {
-                val = nums[l];
-                l++;
-            }else {
-                val = nums[r];
-                r--;
-            }
-            result[i] = val * val;
-        }
-        return result;
+        int i = 0,
+            j = nums.length -1;
         
+        int k = nums.length - 1;
+
+        while(i<=j)
+        {
+            if(Math.abs(nums[i]) > Math.abs(nums[j]))
+            {
+                res[k] = nums[i] * nums[i];
+                i++;
+            }
+            else
+            {
+                res[k] = nums[j] * nums[j];
+                j--;
+            }
+            k--;
+        }
+        return res;
+
     }
 }
 
 
+// public int[] sortedSquares(int[] nums) {
+        
+//         int n = nums.length;
+//         int l = 0,
+//             r = nums.length-1;
 
+//         int[] result = new int[n];
+
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             int val;
+
+//             if(Math.abs(nums[l])>Math.abs(nums[r]))
+//             {
+//                 val = nums[l];
+//                 l++;
+//             }else {
+//                 val = nums[r];
+//                 r--;
+//             }
+//             result[i] = val * val;
+//         }
+//         return result;
+        
+//     }
 
 
 // public int[] sortedSquares(int[] nums) {
