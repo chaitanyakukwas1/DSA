1f0430e9b70c878dc711f9eb06638cc378ac7d95
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-22 23:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (27.45%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,7 +1,56 @@
-import java.util.Arrays;
-
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i = 0,
+            j = nums.length - 1,
+            k = 0;
+        while(k<=j)
+        {
+            if(nums[k]==1)
+            {
+                k = k + 1;
+            } else if(nums[k] == 2)
+            {
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                j = j - 1;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                
+                i = i + 1;
+                k = k + 1;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         Arrays.sort(nums);
+//     }
+// }
